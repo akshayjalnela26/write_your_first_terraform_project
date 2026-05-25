2ecf0e2a9da3bf233f7a1ddac8db1a10f120250d
Akshay Jalnela <akshayjalnela2602@gmail.com>
2026-05-25 09:55:23 +0530

    Fix AMI mapping and update configuration

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -2,23 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 6.46" # Modern AWS Provider line for May 2026
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.15.0" # Modern core Terraform standard
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = "ami-0022cea480ff4920c" # Hardcoded specific AMI ID from AWS
+  instance_type = "t3.micro"               # Modern Free Tier standard
 
   tags = {
     Name = "Terraform_Demo"
   }
 }
-
